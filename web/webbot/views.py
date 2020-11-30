20efe4bcd4372b8b5fda8892f4754be5c314807c
vikaputri <vikaputri909@gmail.com>
2020-11-30 21:23:12 +0700

    Commit Kedua

--- web/webbot/views.py
+++ web/webbot/views.py
@@ -2,10 +2,7 @@ from django.shortcuts import render
 import pandas as pd
 import pickle
 from sklearn.metrics.pairwise import cosine_similarity
-import requests
-from PIL import Image
-from io import BytesIO
-import matplotlib.pyplot as plt
+from urllib.request import urlopen
 
 def index(request):
     return render(request, 'webbot/index.htm')
@@ -13,32 +10,31 @@ def index(request):
 
 def bot_search(request):
     query = request.GET.get('query')
-    book = pd.read_csv("/home/vika/Music/skrip/web/webbot/book_data.csv")    
+    book = pd.read_csv("/home/vika/Music/skrip/web/webbot/book_data.csv")   
 
-    filename = '/home/vika/Music/skrip/web/webbot/tfidf'
-    output = open(filename,'rb')
-    new_dict = pickle.load(output)
-    output.close()
+    try: 
+        filename = '/home/vika/Music/skrip/web/webbot/Model.pkl'
+        output = open(filename,'rb')
+        new_dict = pickle.load(output)
+        output.close()
+        cosine_sim = cosine_similarity(new_dict, new_dict)
 
-    def recommendations(title):
-        cosine_similarities = cosine_similarity(new_dict,  new_dict)
-        books = book[['book_title', 'image_url']]
-        indices = pd.Series(book.index, index = book['book_title']).drop_duplicates()
-             
-        idx = indices[title]
-        sim_scores = list(enumerate(cosine_similarities[idx]))
-        sim_scores = sorted(sim_scores, key = lambda x: x[1], reverse = True)
-        sim_scores = sim_scores[1:6]
-        book_indices = [i[0] for i in sim_scores]
-        recommend = books.iloc[book_indices]
-        for index, row in recommend.iterrows():
+        def get_recommendations(title, cosine_sim=cosine_sim):
+            idx = book.loc[book['book_title'].isin([title])]
+            idx = idx.index
+            sim_scores = list(enumerate(cosine_sim[idx][0]))
+            sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+            sim_scores = sim_scores[1:5]
+            indices = [i[0] for i in sim_scores]
+            judul = book[['book_title', 'image_url']].iloc[indices]
+            hasil = judul.to_dict('records') #dictionarry
+            return hasil
 
-            response = requests.get(row['image_url'])
-            img = Image.open(BytesIO(response.content))
-            plt.figure()
-            plt.imshow(img)
-            plt.title(row['book_title'])
+        result = get_recommendations(query)
 
-    hasil = recommendations(query)
+        ans = result
 
-    return render(request, 'webbot/index.htm', {'hasil': hasil,'query': query})
+    except Exception as e:
+        ans = {'book_title': 'NOT FOUND'}
+
+    return render(request, 'webbot/index.htm', {'ans': ans,'query': query})
